006f9878147b47fdea90efcc24ab2d40edf6696f
ALIREZA NOROOZI <79312684+Alirzeanoroozi@users.noreply.github.com>
2024-07-16 11:05:54 +0330

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ParaAntiProt',
+    version='1.0.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Alireza Noroozi',
+    author_email='noroozi.alirezaa@gmail.com',
+    description=''
+)
